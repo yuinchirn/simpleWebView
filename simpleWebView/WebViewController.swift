fc5e44fe5a730d75573667a4cbbe63f22419b010
yuinchirn <l.serah.yuinch@gmail.com>
2015-04-27 02:13:36 +0900

    add prev&nextBtn and SocialPost function

--- simpleWebView/WebViewController.swift
+++ simpleWebView/WebViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class WebViewController: UIViewController, UIWebViewDelegate {
 
@@ -16,32 +17,73 @@ class WebViewController: UIViewController, UIWebViewDelegate {
     // 初期URL
     let initialUrl = NSURL(string: "https://www.google.co.jp")
     
+    
     /* 最初の表示時に呼ばれるメソッド */
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // webViewのデリゲートを設定
-        webView.delegate = self
+         self.webView.delegate = self
         
         // 初期URLで読み込み
         let request = NSURLRequest(URL: initialUrl!)
-        webView.loadRequest(request)
+        self.webView.loadRequest(request)
+    }
+    
+    /* 戻るボタン押下時に呼ばれるメソッド */
+    @IBAction func prevBtn(sender: AnyObject) {
+        
+        if self.webView.canGoBack {
+            self.webView.goBack() // 戻る
+        }
+    }
+    
+    /* 進むボタン押下時に呼ばれるメソッド */
+    @IBAction func nextBtn(sender: AnyObject) {
+        
+        if self.webView.canGoForward {
+            self.webView.goForward() // 進む
+        }
+    }
+    
+    /* facebookボタン押下時に呼ばれるメソッド */
+    @IBAction func facebookBtn(sender: AnyObject) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook) {
+            showPostView(serviceType: SLServiceTypeFacebook)
+        }
     }
 
+    /* twitterボタン押下時に呼ばれるメソッド */
+    @IBAction func twitterBtn(sender: AnyObject) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            showPostView(serviceType: SLServiceTypeTwitter)
+        }
+    }
+    
+    /* 投稿画面を表示 */
+    func showPostView(#serviceType:String){
+        
+        var controller = SLComposeViewController(forServiceType: serviceType)
+        
+        // 表示中のページURLをセット
+        let link: NSURL? = self.webView?.request?.URL
+        if let shareUrl = link {
+            controller.addURL(shareUrl)
+        }
+        
+        // 表示中のページURLのタイトルをセット
+        let title: String? = self.webView.stringByEvaluatingJavaScriptFromString("document.title")
+        if let shareTitle = title {
+            controller.setInitialText(shareTitle)
+        }
+        
+        // 表示
+        presentViewController(controller, animated: true, completion: {})
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
